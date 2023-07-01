4516682ffabe8048995099ee9ee426d7b8b9eb1b
Lionelapex <126233514+Lionelapex@users.noreply.github.com>
2023-07-01 15:40:58 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+const race=[ 'black', 'white', 'coloured', 'Indian ']
+
+const language={
+  type:"zulu", 
+  type2:"English ", 
+  type3:"Afrikaans "
+}
+const country ={
+  name:"south Africa ", 
+  location : "africa", 
+  province: 9,
+  mixedEconomy:true, 
+  races:race, 
+  languages:language 
+}
+const show= country.name="me"
